4d7aaa31e4ebb387b5151cd4ebae9b8ca7e9e09b
E4EYZGMP6YVLIVJ\Administrator <948289131@qq.com>
2016-11-28 20:51:27 +0800

    www类讲解

---
+++ WWW/Assets/Scripts/Www.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Www : MonoBehaviour {
+
+    //本地贴图
+    Texture localImage;
+    //服务器端贴图
+    Texture serverImage;
+
+    //加载本地贴图
+    IEnumerator localLoad() 
+    {
+        if (localImage==null)
+        {
+            WWW local = new WWW("file://"+Application.dataPath + "/Images/1.jpg");
+            yield return local;
+            localImage = local.texture;
+        }
+        gameObject.GetComponent<Renderer>().material.mainTexture = localImage;
+
+    //加载本地服务器端贴图
+    }
+    IEnumerator serverLoad() 
+    {
+        if (serverImage==null)
+        {
+            WWW server = new WWW("http://localhost/Images/2.jpg");
+            yield return server;
+            serverImage = server.texture;
+        }
+        gameObject.GetComponent<Renderer>().material.mainTexture = serverImage;
+    }
+    void OnGUI() 
+    {
+        if (GUILayout.Button("点击切换本地贴图"))
+        {
+            StartCoroutine(localLoad());
+        }
+        if (GUILayout.Button("点击切换服务器贴图"))
+        {
+            StartCoroutine(serverLoad());
+        }
+    }
+}
