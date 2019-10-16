e3b5ebba795d45726207b9fa23d3495a0d306271
min.cai <caimin.caimin@bytedance.com>
2019-10-16 12:51:14 +0800

    新增自動打包和動畫機

---
+++ WWW/Assets/Editor/BatchMode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEditor;
+
+public class BatchMode{
+ static List<string> levels = new List<string>();
+ 
+    public static void BuildAndroid()
+    {
+        foreach (EditorBuildSettingsScene item in EditorBuildSettings.scenes)
+        {
+            if (!item.enabled)
+            {
+                continue;
+            }
+            levels.Add(item.path);
+        }
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+        string res = BuildPipeline.BuildPlayer(levels.ToArray(), "WWW.apk", BuildTarget.Android, BuildOptions.None);
+        if (res.Length>0)
+        {
+            throw new Exception("BuildPlayer failure:" + res);
+        }
+    }   	
+}
